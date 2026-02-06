93a017c210a3774e43a5caeaf8cb6bfa11faeb05
Kai Pereira <kaipereira2020@gmail.com>
2026-02-06 06:57:09 -0800

    circles and formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
-<div class="flex justify-center w-full pt-24 drop-shadow">
+<div class="flex flex-col items-center w-full pt-36">
 	<h1 class="text-7xl font-bold">Ship Projects, Get Prizes</h1>
+
+	<div class="grid grid-rows-2">
+		<img src="/images/normal_circle.svg" class="w-xs"/>
+		<img src="/images/circly_circle.svg" class="w-xs"/>
+		<img src="/images/spiky_circle.svg" class="w-xs"/>
+	</div>
 </div>
 
-<img src="/images/tiny4fsk.png" class="absolute w-lg right-0 bottom-0" />
-<img src="/images/icepi.png" class="absolute -left-30 -rotate-20 w-md top-36" />
-<img src="/images/cheetahmx4mini.png" class="absolute bottom-0 w-md" />
+<img src="/images/tiny4fsk.png" class="absolute w-lg right-0 bottom-0 drop-shadow" />
+<img src="/images/icepi.png" class="absolute -left-30 -rotate-20 w-md top-36 drop-shadow" />
+<img src="/images/cheetahmx4mini.png" class="absolute -bottom-24 -left-12 w-md drop-shadow" />
+

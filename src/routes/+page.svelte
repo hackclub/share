260ae70bd5beb9b1fadbcbbd7cd2c632ee452dbd
Kai Pereira <kaipereira2020@gmail.com>
2026-02-06 15:15:49 -0800

    organizing all the circles and text font changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,29 @@
 <div class="flex flex-col items-center w-full pt-36">
 	<h1 class="text-7xl font-bold">Ship Projects, Get Prizes</h1>
 
-	<div class="grid grid-rows-2">
-		<img src="/images/normal_circle.svg" class="w-xs"/>
-		<img src="/images/circly_circle.svg" class="w-xs"/>
-		<img src="/images/spiky_circle.svg" class="w-xs"/>
+	<div class="flex justify-around gap-24 mt-24">
+		<div class="tier bg-[url('/images/normal_circle.svg')]">
+			<p class="text-xl">Tier 1</p>
+			<span class="mt-4">10 Stars or</span>
+			<span>50 upvotes reddit</span>
+			<span>50 upvotes HN</span>
+		</div>
+		<div class="tier bg-[url('/images/circly_circle.svg')] mt-12">
+			<p class="text-xl">Tier 1</p>
+			<span class="mt-4">10 Stars or</span>
+			<span>50 upvotes reddit</span>
+			<span>50 upvotes HN</span>
+		</div>
+		<div class="tier bg-[url('/images/spiky_circle.svg')]">
+			<p class="text-xl">Tier 1</p>
+			<span class="mt-4">10 Stars or</span>
+			<span>50 upvotes reddit</span>
+			<span>50 upvotes HN</span>
+		</div>
 	</div>
 </div>
 
-<img src="/images/tiny4fsk.png" class="absolute w-lg right-0 bottom-0 drop-shadow" />
-<img src="/images/icepi.png" class="absolute -left-30 -rotate-20 w-md top-36 drop-shadow" />
-<img src="/images/cheetahmx4mini.png" class="absolute -bottom-24 -left-12 w-md drop-shadow" />
+<img src="/images/tiny4fsk.png" class="absolute w-lg right-0 bottom-0 drop-shadow -z-1" />
+<img src="/images/icepi.png" class="absolute -left-30 -rotate-20 w-md top-36 drop-shadow -z-1" />
+<img src="/images/cheetahmx4mini.png" class="absolute -bottom-24 -left-12 w-md drop-shadow -z-1" />
 

612903755e5d838ec32d616dfe2a9c83a180ae1a
Kai Pereira <kaipereira2020@gmail.com>
2026-02-06 06:28:51 -0800

    adding a bunch of stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-Hello world!
+<div class="flex justify-center w-full pt-24 drop-shadow">
+	<h1 class="text-7xl font-bold">Ship Projects, Get Prizes</h1>
+</div>
+
+<img src="/images/tiny4fsk.png" class="absolute w-lg right-0 bottom-0" />
+<img src="/images/icepi.png" class="absolute -left-30 -rotate-20 w-md top-36" />
+<img src="/images/cheetahmx4mini.png" class="absolute bottom-0 w-md" />

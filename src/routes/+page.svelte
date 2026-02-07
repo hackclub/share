d1eefd928ac2aebca798fcc5dd5c1db19fba2a17
Kai Pereira <kaipereira2020@gmail.com>
2026-02-07 11:37:52 -0800

    RAHH funtime :D

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,33 @@
-<div class="flex flex-col items-center w-full pt-36">
+<div class="flex justify-end py-12 px-22">
+	<a href="/docs" class="underline decoration-wavy">Learn how to Ship →</a>
+</div>
+<div class="flex flex-col items-center w-full pt-20">
 	<h1 class="text-7xl font-bold">Ship Projects, Get Prizes</h1>
 
 	<div class="flex justify-around gap-24 mt-24">
 		<div class="tier bg-[url('/images/normal_circle.svg')]">
 			<p class="text-xl">Tier 1</p>
-			<span class="mt-4">10 Stars or</span>
+			<span class="mt-6">10 Stars or</span>
 			<span>50 upvotes reddit</span>
 			<span>50 upvotes HN</span>
 		</div>
 		<div class="tier bg-[url('/images/circly_circle.svg')] mt-12">
 			<p class="text-xl">Tier 1</p>
-			<span class="mt-4">10 Stars or</span>
+			<span class="mt-6">10 Stars or</span>
 			<span>50 upvotes reddit</span>
 			<span>50 upvotes HN</span>
 		</div>
 		<div class="tier bg-[url('/images/spiky_circle.svg')]">
 			<p class="text-xl">Tier 1</p>
-			<span class="mt-4">10 Stars or</span>
+			<span class="mt-6">10 Stars or</span>
 			<span>50 upvotes reddit</span>
 			<span>50 upvotes HN</span>
 		</div>
 	</div>
+
+	<a class="button" href="https://forms.google.com" target="_blank" rel="noreferrer">
+		Submit a Project →
+	</a>
 </div>
 
 <img src="/images/tiny4fsk.png" class="absolute w-lg right-0 bottom-0 drop-shadow -z-1" />

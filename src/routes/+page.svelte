fe5d36189abd2778644441acb9180afd2b1a0a47
Kai Pereira <kaipereira2020@gmail.com>
2026-02-07 17:58:19 -0800

    adding the proper tiers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,19 +11,18 @@
 			<p class="text-xl">Tier 1</p>
 			<span class="mt-6">10 Stars or</span>
 			<span>50 upvotes reddit</span>
-			<span>50 upvotes HN</span>
 		</div>
 		<div class="tier bg-[url('/images/circly_circle.svg')] mt-12">
-			<p class="text-xl">Tier 1</p>
-			<span class="mt-6">10 Stars or</span>
-			<span>50 upvotes reddit</span>
+			<p class="text-xl">Tier 2</p>
+			<span class="mt-6">35 Stars or</span>
+			<span>150 upvotes reddit or</span>
 			<span>50 upvotes HN</span>
 		</div>
 		<div class="tier bg-[url('/images/spiky_circle.svg')]">
-			<p class="text-xl">Tier 1</p>
-			<span class="mt-6">10 Stars or</span>
-			<span>50 upvotes reddit</span>
-			<span>50 upvotes HN</span>
+			<p class="text-xl">Tier 3</p>
+			<span class="mt-6">75 Stars or</span>
+			<span>550 upvotes reddit</span>
+			<span>100 upvotes HN</span>
 		</div>
 	</div>
 
@@ -37,5 +36,4 @@
 </a>
 <img src="/images/tiny4fsk.png" class="absolute w-lg right-0 bottom-0 drop-shadow -z-1" />
 <img src="/images/icepi.png" class="absolute -left-30 -rotate-20 w-md top-46 drop-shadow -z-1" />
-<img src="/images/cheetahmx4mini.png" class="absolute -bottom-24 -left-12 w-md drop-shadow -z-1" />
-
+<img src="/images/cheetahmx4mini.png" class="absolute bottom-0 -left-12 w-md drop-shadow -z-1" />

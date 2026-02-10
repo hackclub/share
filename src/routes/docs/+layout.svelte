99ed071bb281e0e1615f4d6828ee7377230a8647
Kai Pereira <kaipereira2020@gmail.com>
2026-02-09 18:58:28 -0800

    FINALLY jank workaround but so is life :/

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -4,13 +4,14 @@
 	let { children } = $props();
 </script>
 
-<div class="py-12 px-22">
-	<a href="/">← Return home :D</a>
-</div>
+<div class="docs"> <!--JANK workaround to make sure the CSS doesn't get preloaded :((( -->
+	<div class="py-12 px-22">
+		<a href="/">← Return home :D</a>
+	</div>
 
-<div class="w-full flex justify-center px-12 pb-20">
-	<div class="w-full max-w-4xl flex flex-col gap-6 relative">
-		{@render children()}
+	<div class="w-full flex justify-center px-12 pb-20">
+		<div class="w-full max-w-4xl flex flex-col gap-6 relative">
+			{@render children()}
+		</div>
 	</div>
 </div>
-
